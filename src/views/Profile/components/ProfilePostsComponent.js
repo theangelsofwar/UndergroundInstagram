9fcda895819dc39cd5f3a36944f932b5ed82d46e
angiechangpagne@gmail.com <angiechangpagne@gmail.com>
2020-03-01 11:50:31 -0800

    containers

---
+++ src/views/Profile/components/ProfilePostsComponent.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { FlatList, Dimensions } from 'react-native';
+import PropTypes from 'prop-types';
+import PostBox from '../../../assets/theme/components/PostBox';
+
+const ProfilePostsComponent = ({
+  t,
+  data,
+  initialScrollIndex,
+  onPressDetachedLike,
+  onPressOthers,
+}) => (
+  <FlatList 
+  data={data}
+  keyExtractor={( item, index ) => index.toString()}
+  renderItem ={({ item }) => (
+    <PostBox
+    t={t}
+    data={item}
+    onPressDetachedLike={onPressDetachedLike}
+    onPressOthers={onPressOthers}
+    />
+  )}
+  initialScrollIndex={initialScrollIndex}
+  getItemLayout={(item, index) => ({
+    length: Dimensions.get(`window`).height,
+    offset: Dimensions.get(`screen`).height*index*.95,
+    index,
+
+  })}
+  />
+);
+
+ProfilePostsComponent.propTypes={
+  t: PropTypes.func.isRequired,
+  data: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  initialScrollIndex: PropTypes.number.isRequired,
+  onPressDetachedLike: PropTypes.func.isRequired,
+  onPressOthers: PropTypes.func.isRequired,
+};
+
+export default ProfilePostsComponent;
---
+++ src/views/Profile/containers/ProfileContainer.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import ProfileComponent from '../components/ProfileComponent';
+import { profileSpotLights, profilePersonalPosts, profileTaggedImages } from '../../../mocks';
+
+
+const ProfileContainer = ({ navigation, dispatch, creators, personReducer, t }) => {
+  const [navigationState, setNavigationState] = useState({
+  index: 0,
+  routes: [{ key: `personal`}, { key: `tagged` }],
+});
+
+return (
+  <ProfileComponent
+   t={t}
+   navigation={navigation}
+   profileSpotLights={profileSpotLights}
+   profilePersonalPosts={profilePersonalPosts}
+   profileTaggedImages={profileTaggedImages}
+   naviagtionState={navigationState}
+   setNavigationState={setNavigationState}
+   />
+  );
+};
+
+ProfileContainer.propTypes = {
+  navigation: PropTypes.shape({}).isRequired,
+  dispatch: PropTypes.func.isRequired,
+  creators: PropTypes.shape({}).isRequired,
+  personReducer: PropTypes.shape({}).isRequired,
+  t: PropTypes.func.isRequired,
+};
+
+export default ProfileContainer;
