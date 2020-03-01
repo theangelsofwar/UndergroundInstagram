143221c10737d3a9799553a38e02f209483fc21c
angiechangpagne@gmail.com <angiechangpagne@gmail.com>
2020-03-01 10:55:04 -0800

    Profile Edit

---
+++ src/views/Profile/components/ProfileEditComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
+import styles from '../../../assets/theme/styles/app';
+
+const ProfileEditComponent = ({ onPress, message }) => (
+  <TouchableOpacity style={styles.centeredItemsContainer} onPress={onPress}>
+    <Text> {message} </Text>
+  </TouchableOpacity>
+);
+
+ProfileEditComponent.propTypes = {
+  onPress: PropTypes.func.isRequired,
+  message: PropTypes.string.isRequired,
+};
+
+export default ProfileEditComponent;
