579706a514bcbb90a00383f94e0279f2fc4456c9
angiechangpagne@gmail.com <angiechangpagne@gmail.com>
2020-02-29 18:51:02 -0800

    reactotron

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider as ReduxProvider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { I18nextProvider } from 'react-i18next';
+import AppContainer from './routes';
+import reactotron from './config/reactotronConfig';
+import i18n from './assets/locales/i18n';
+
+const store = __DEV__
+  ? createStore(reducers, reactotron.createEnhancer(applyMiddleware(thunk)))
+  : createStore(reducers, applyMiddleware(thunk));
+
+  const App = () => (
+    <ReduxProvider store={store}>
+      <I18nextProvider i18n={i18n}>
+        <AppContainer />
+      </I18nextProvider>
+    </ReduxProvider>
+  );
+  export default App;
---
+++ src/routes.js
@@ -0,0 +1,65 @@
+import {
+  createAppContainer,
+  createStackNavigator,
+  createBottomTabNavigator,
+  createSwitchNavigator,
+} from 'react-navigation';
+import React from 'react';
+import Home from './views/Home';
+import { Profile, ProfileEdit, ProfilePosts } from './views/Profile';
+import Icon from './assets/theme/components/Icon';
+import { constants } from './assets/theme/styles/app';
+
+const ProfileStack = createStackNavigator(
+  {
+    Profile: { screen: Profile },
+    ProfileEdit: { screen: ProfileEdit },
+  },
+  {
+    mode: `modal`,headerMode: `none`
+  },
+);
+
+const ProfileMainStack = createStackNavigator(
+  { 
+    Profile: { screen: ProfileStack },
+    ProfilePosts: { screen: ProfilePosts },
+  },
+);
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Home, 
+    Profile: ProfileMainStack,
+  },
+  {
+    tabBarOptions: {
+      showLabel: false,
+    },
+    defaultNavigationOptions: ({ navigation }) => {
+      const { routeName } =navigation.state;
+      const { activeItemColor , inactiveItem} = constants;
+      return {
+        tabBarIcon: ({ focused }) => (
+          <Icon name={`${routeName}Icon`} fill={focused ? activeItemColor: inactiveItemColor }/>
+        ),
+      };
+    },
+    initialRouteName: `Profile`,
+  },
+);
+
+const AppStackNavigator = createStackNavigator(
+  {
+    TabNavigator,
+  },
+  { headerMode: `none` },
+);
+
+const AppSwitchNavigator=createSwitchNavigator({
+  Main: AppStackNavigator,
+});
+
+const AppContainer = createAppContainer(AppSwitchNavigator);
+
+export default AppContainer;
